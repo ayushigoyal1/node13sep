3c5f3d07b07653e3c666245f03fb4137f06e3141
unknown <ayushigoyal1303@gmail.com>
2024-09-13 16:44:11 +0530

    after change

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,7 @@ const router=express.Router()
 router.get('/users',(req,res)=>{
     usercontroller.getAllUser(req,res)
 })
-
+router.get('/user/:id',(req,res)=>{
+    usercontroller.getUser(req,res)
+})
 module.exports=router
